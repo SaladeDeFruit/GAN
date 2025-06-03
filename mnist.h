e16029eb5567bb4fad25a859188cdc1b70b5deaa
Salade <82703399+SaladeDeFruit@users.noreply.github.com>
2025-06-03 23:09:56 +0200

    Update mnist.h

--- mnist.h
+++ mnist.h
@@ -1,6 +1,16 @@
 #include "tenseur.h"
 #include <stdio.h>
 #define MNIST_T "./MNIST/mnist_train.csv"
+#define MNIST_T0 "./MNIST/mnist0.csv"
+#define MNIST_T1 "./MNIST/mnist1.csv"
+#define MNIST_T2 "./MNIST/mnist2.csv"
+#define MNIST_T3 "./MNIST/mnist3.csv"
+#define MNIST_T4 "./MNIST/mnist4.csv"
+#define MNIST_T5 "./MNIST/mnist5.csv"
+#define MNIST_T6 "./MNIST/mnist6.csv"
+#define MNIST_T7 "./MNIST/mnist7.csv"
+#define MNIST_T8 "./MNIST/mnist8.csv"
+#define MNIST_T9 "./MNIST/mnist9.csv"
 #define MNIST_TEST "./MNIST/mnist_test.csv"
 #define TAILLE_IMAGE 28
 
